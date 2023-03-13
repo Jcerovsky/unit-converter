8f4eda093a81651b11363c58fd816809220dc9fa
Marek Kobida <marek.kobida@gmail.com>
2023-03-13 14:22:51 +0100

    100% natypovaný súbor

--- index.ts
+++ index.ts
@@ -1,45 +1,58 @@
-const convertBtn = document.getElementById('convert');
+// "getElementById" vracia "HTMLElement" a my potrebujeme "HTMLInputElement", ktorý drží "value"
+// v typescript existuje "as" – typová konverzia, ale skúsime toto ošetriť nižšie v kóde
 const inputEl = document.getElementById('input');
 const meterEl = document.getElementById('meters');
 const literEl = document.getElementById('liters');
 const kilogramEl = document.getElementById('kilograms');
 
-const meterToFeet = num => {
+// vstup "num" je číslo ale funkcia "toFixed" už vracia "string"
+const meterToFeet = (num: number): string => {
   return (num * 3.281).toFixed(3);
 };
 
-const feetToMeter = num => {
+const feetToMeter = (num: number): string => {
   return (num / 3.281).toFixed(3);
 };
 
-const litersToGalons = num => {
+const litersToGalons = (num: number): string => {
   return (num * 0.264).toFixed(3);
 };
 
-const galonsToLiters = num => {
+const galonsToLiters = (num: number): string => {
   return (num / 0.264).toFixed(3);
 };
 
-const kgToPound = num => {
+const kgToPound = (num: number): string => {
   return (num * 2.204).toFixed(3);
 };
-const poundToKg = num => {
+const poundToKg = (num: number): string => {
   return (num / 2.204).toFixed(3);
 };
 
-const isMoreThanOne = num => {
-  return num == 1 ? '' : 's';
+// znak rovnosti "==" nikdy nepoužívaj – vždy používaj "==="
+const isMoreThanOne = (num: number): string => {
+  return num === 1 ? '' : 's';
 };
 
-// use coverBtn.addEventListener if you want to convert after press of the button
-inputEl.addEventListener('input', () => {
-  const inputValue = inputEl.value;
-  meterEl.innerHTML = `${inputValue} meter${isMoreThanOne(inputValue)} = ${meterToFeet(inputValue)} feet | 
-    ${inputValue} feet = ${feetToMeter(inputValue)} meters`;
+// "input event" existuje pri "HTMLInputElement", takže to musíme ošetriť, aby náhodou "inputEl" nebol "div"
+if (inputEl instanceof HTMLInputElement) {
+  inputEl.addEventListener('input', () => {
+    // to plus urobí zo "string" – "number", čo vlastne potrebuješ pre potreby všetkých tých definovaných funkcií
+    // avšak kľudne použi "parseInt(inputEl.value)" – myslím, že to je úplne to isté :-)
+    const inputValue = +inputEl.value;
 
-  literEl.innerHTML = `${inputValue} liter${isMoreThanOne(inputValue)} = ${litersToGalons(inputValue)} galons | 
-    ${inputValue} galon${isMoreThanOne(inputValue)} = ${galonsToLiters(inputValue)} liters`;
+    // "meterEl" môže byť "null" a preto pomocou podmienky skontrolujeme, či je "element" definovaný alebo nie
+    // namiesto "if" som použil "ternárny operátor" – "&&" je AND a "||" je OR
+    meterEl &&
+      (meterEl.innerHTML = `${inputValue} meter${isMoreThanOne(inputValue)} = ${meterToFeet(inputValue)} feet | 
+    ${inputValue} feet = ${feetToMeter(inputValue)} meters`);
 
-  kilogramEl.innerHTML = `${inputValue} kilogram${isMoreThanOne(inputValue)} = ${kgToPound(inputValue)} pounds | 
-    ${inputValue} pound${isMoreThanOne(inputValue)} = ${poundToKg(inputValue)} kilograms`;
-});
+    literEl &&
+      (literEl.innerHTML = `${inputValue} liter${isMoreThanOne(inputValue)} = ${litersToGalons(inputValue)} galons | 
+    ${inputValue} galon${isMoreThanOne(inputValue)} = ${galonsToLiters(inputValue)} liters`);
+
+    kilogramEl &&
+      (kilogramEl.innerHTML = `${inputValue} kilogram${isMoreThanOne(inputValue)} = ${kgToPound(inputValue)} pounds | 
+    ${inputValue} pound${isMoreThanOne(inputValue)} = ${poundToKg(inputValue)} kilograms`);
+  });
+}
